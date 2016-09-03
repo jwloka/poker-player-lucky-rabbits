0a71e5061a630a77e193e7b3443a9c1753f35e42
bbvoerle <bart.van.oerle@gmail.com>
2016-09-03 13:13:49 +0200

    Fix PairRule

--- src/main/java/org/leanpoker/player/rules/PairRule.java
+++ src/main/java/org/leanpoker/player/rules/PairRule.java
@@ -1,6 +1,7 @@
 package org.leanpoker.player.rules;
 
 import java.util.HashMap;
+import java.util.List;
 import org.leanpoker.player.model.Card;
 import org.leanpoker.player.model.GameState;
 
@@ -18,11 +19,11 @@ public class PairRule extends Rule {
 
     @Override
     public int apply() {
-        Map<String, Integer> rankMap = new HashMap<>();
-        String curRank = null;
-        for (Card cur : state.getOurPokerBot().getHole_cards()) {
-            curRank = cur.getRank();
-            if (rankMap.containsKey(curRank)) {
+        List<Card> cards = state.getOurPokerBot().getHole_cards();
+        if(cards.size() == 2){
+            Card card1 = cards.get(0);
+            Card card2 = cards.get(1);      
+            if(card1.getRank().equalsIgnoreCase(card2.getRank())){
                 return descision.getRaise();
             }
         }
