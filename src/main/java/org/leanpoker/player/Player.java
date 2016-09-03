9558c4e727b5ec05f0f17358ffc1b5e8379da4ff
Jan Wloka <jan.wloka@gmail.com>
2016-09-03 14:51:42 +0200

    Look at our cards only

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -28,11 +28,11 @@ public class Player {
         Cards bothCards = new Cards(both);
         Cards ourCards = new Cards(cards);
 
-        Integer factor = bothCards.makeRaiseFactor();
+        Integer factor = ourCards.makeRaiseFactor();
         if (factor > 0) {
             return new Raises().raise(state, factor);
         }
-        else if (bothCards.hasSameColor()) {
+        else if (ourCards.hasSameColor()) {
             return new Raises().check(state);
         }
         else {
