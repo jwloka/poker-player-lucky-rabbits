c94f7623f1158293d807d6b1b30b08d66b482352
Balazs Horvath <hbala12@gmail.com>
2016-09-03 14:09:19 +0200

    color tests.

--- src/test/java/org/leanpoker/player/model/CardsTest.java
+++ src/test/java/org/leanpoker/player/model/CardsTest.java
@@ -8,7 +8,7 @@ import java.util.List;
 import static org.junit.Assert.*;
 
 /**
- * Created by titko on 9/3/2016.
+ * Created by Balazs on 9/3/2016.
  */
 public class CardsTest {
 
@@ -32,5 +32,28 @@ public class CardsTest {
         Cards cards = new Cards(elements);
         assertTrue(cards.hasPair());
     }
+
+    @Test
+    public void threeNoPairFalse() {
+        List<Card>  elements = Arrays.asList(new Card("6", "spade"), new Card("9", "diamonds"), new Card("7", "hearts"));
+        Cards cards = new Cards(elements);
+        assertFalse(cards.hasPair());
+    }
+
+    @Test
+    public void testTwoCardsHasSameColorTrue() {
+        List<Card>  elements = Arrays.asList(new Card("7", "spade"), new Card("6", "spade"));
+        Cards cards = new Cards(elements);
+        assertTrue(cards.hasSameColor());
+    }
+
+    @Test
+    public void testTwoCardsHasSameColorFalse() {
+        List<Card>  elements = Arrays.asList(new Card("7", "hearts"), new Card("6", "spade"));
+        Cards cards = new Cards(elements);
+        assertFalse(cards.hasSameColor());
+    }
+
+
 
 }
