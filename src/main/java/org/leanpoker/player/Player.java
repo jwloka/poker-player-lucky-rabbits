af01578c70b2b79d45acf63286c5da15dc4a3022
Jan Wloka <jan.wloka@gmail.com>
2016-09-03 16:40:43 +0200

    Refactor in.

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -3,7 +3,7 @@ package org.leanpoker.player;
 import com.google.gson.JsonElement;
 import org.leanpoker.player.data.Parser;
 import org.leanpoker.player.model.GameState;
-import org.leanpoker.player.rules.BartsRuleFactory;
+import org.leanpoker.player.rules.HighBlindsRule;
 import org.leanpoker.player.rules.Rule;
 import org.leanpoker.player.rules.StableRule;
 
@@ -18,9 +18,13 @@ public class Player {
         GameState state = parser.parse(request);
 
 
-        Rule activeRule = new BartsRuleFactory().conservativeOrDaring(state);
-
-        return  activeRule.apply();
+        Rule result;
+        if ((state.getOurPokerBot().getStack() + state.getBet_index()) / state.getSmall_blind() <= 10) {
+            result = new HighBlindsRule(state);
+        } else {
+            result = new StableRule(state);
+        }
+        return  result.apply();
     }
 
     public static void showdown(JsonElement game) {
