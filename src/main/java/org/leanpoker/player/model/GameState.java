1c256ee87c39a700692dcecb9b1118b025ddbc00
bbvoerle <bart.van.oerle@gmail.com>
2016-09-03 11:17:05 +0200

    GameState with all data

--- src/main/java/org/leanpoker/player/model/GameState.java
+++ src/main/java/org/leanpoker/player/model/GameState.java
@@ -1,13 +1,125 @@
 package org.leanpoker.player.model;
 
-public class GameState {
-    private PokerBot user;
+import java.util.List;
 
-    public PokerBot getUser() {
-        return new PokerBot();
+public class GameState {
+    private int tournamentId;
+    private int gameId;
+    private int round;
+    private int betIndex;
+    private int smallBlind;
+    private int currentBuyIn;
+    private int pot;
+    private int minimumRaise;
+    private int dealerId;
+    private int orbits;
+    private int inActionId;
+    private List<PokerBot> allBots;
+    private List<Card> communityCards;
+
+    public int getTournamentId() {
+        return tournamentId;
+    }
+
+    public void setTournamentId(int tournamentId) {
+        this.tournamentId = tournamentId;
+    }
+
+    public int getGameId() {
+        return gameId;
+    }
+
+    public void setGameId(int gameId) {
+        this.gameId = gameId;
+    }
+
+    public int getRound() {
+        return round;
+    }
+
+    public void setRound(int round) {
+        this.round = round;
+    }
+
+    public int getBetIndex() {
+        return betIndex;
+    }
+
+    public void setBetIndex(int betIndex) {
+        this.betIndex = betIndex;
+    }
+
+    public int getSmallBlind() {
+        return smallBlind;
+    }
+
+    public void setSmallBlind(int smallBlind) {
+        this.smallBlind = smallBlind;
     }
 
     public int getCurrentBuyIn() {
-        return 0;
+        return currentBuyIn;
     }
+
+    public void setCurrentBuyIn(int currentBuyIn) {
+        this.currentBuyIn = currentBuyIn;
+    }
+
+    public int getPot() {
+        return pot;
+    }
+
+    public void setPot(int pot) {
+        this.pot = pot;
+    }
+
+    public int getMinimumRaise() {
+        return minimumRaise;
+    }
+
+    public void setMinimumRaise(int minimumRaise) {
+        this.minimumRaise = minimumRaise;
+    }
+
+    public int getDealerId() {
+        return dealerId;
+    }
+
+    public void setDealerId(int dealerId) {
+        this.dealerId = dealerId;
+    }
+
+    public int getOrbits() {
+        return orbits;
+    }
+
+    public void setOrbits(int orbits) {
+        this.orbits = orbits;
+    }
+
+    public int getInActionId() {
+        return inActionId;
+    }
+
+    public void setInActionId(int inActionId) {
+        this.inActionId = inActionId;
+    }
+
+    public List<PokerBot> getAllBots() {
+        return allBots;
+    }
+
+    public void setAllBots(List<PokerBot> allBots) {
+        this.allBots = allBots;
+    }
+
+    public List<Card> getCommunityCards() {
+        return communityCards;
+    }
+
+    public void setCommunityCards(List<Card> communityCards) {
+        this.communityCards = communityCards;
+    }
+
+   
 }
