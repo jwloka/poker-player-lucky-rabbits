7b08742f40f06b227f65ffa1d07c01a7b0abe8b3
bbvoerle <bart.van.oerle@gmail.com>
2016-09-03 10:41:24 +0200

    User class

--- src/main/java/org/leanpoker/player/model/User.java
+++ src/main/java/org/leanpoker/player/model/User.java
@@ -1,10 +1,62 @@
 package org.leanpoker.player.model;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class User {
+    private int id;
+    private String status;
+    private String version;
+    private int stack;
+    private int bet;
+    private List<Card> cards = new ArrayList<Card>();
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public int getStack() {
+        return stack;
+    }
+
+    public void setStack(int stack) {
+        this.stack = stack;
+    }
+
+    public int getBet() {
+        return bet;
+    }
+
+    public void setBet(int bet) {
+        this.bet = bet;
+    }
+
     public List<Card> getCards() {
-        return Collections.emptyList();
+        return cards;
+    }
+
+    public void setCards(List<Card> cards) {
+        this.cards = cards;
     }
 }
